29e626b11645cb48fa4cfa93d8cdbed0c5eb6e9c
zain <zainmd.htm@gmail.com>
2025-01-02 21:56:40 +0500

    Add files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Roboto} from "next/font/google";
+// import { Roboto} from "next/font/google";
 
 // const myRoboto = Roboto({
     
